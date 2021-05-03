62f01d1b6410794969654ec34a227f790872ab6c
Bhavul Gauri <bhavul93@gmail.com>
2021-05-04 02:25:06 +0530

    Make it super-useful for everyone

--- st.py
+++ st.py
@@ -6,8 +6,13 @@ import pandas as pd
 import time
 import pytz
 
-st.set_page_config(layout="wide")
 
+IST = pytz.timezone('Asia/Kolkata')
+
+##############
+# ST CONFIGS
+##############
+st.set_page_config(layout="wide")
 st.markdown(
     """
     <style>
@@ -21,11 +26,20 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-IST = pytz.timezone('Asia/Kolkata')
+##############
+# FUNCTIONS
+##############
 
-def check():
+@st.cache
+def get_district_id_map():
+    df = pd.read_csv('./district_mapping.csv')
+    district_id_map = pd.Series(df['district id'].values, index=df['district name']).to_dict()
+    return district_id_map
+
+
+
+def check(list_of_district_ids, min_age_limit):
     NUMPERIODS = 4  # check for next 9 days
-    DELHI_DISTRICT_CODES = [141, 145, 140, 146, 147, 143, 144, 149, 150, 142, 148, 661, 265]
     all_centers = []
     covaxin_centers = []
     covishield_centers = []
@@ -33,7 +47,7 @@ def check():
     today_date = datetime.today()
     date_list = [today_date + timedelta(days=3*x) for x in range(NUMPERIODS)]
     date_str_list = [x.strftime("%d-%m-%Y") for x in date_list]
-    for district_id in DELHI_DISTRICT_CODES:
+    for district_id in list_of_district_ids:
         for temp_date in date_str_list:
             url = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={district_id}&date={temp_date}"    
             payload={}
@@ -53,9 +67,9 @@ def check():
                     for session in center['sessions']:
                         vaccine = session['vaccine']
                         available_capacity = session['available_capacity']
-                        min_age_limit = session['min_age_limit']
+                        min_age_limit_session = session['min_age_limit']
                         date = session['date']
-                        if vaccine == 'COVAXIN' and available_capacity > 0 and min_age_limit == 18:
+                        if vaccine == 'COVAXIN' and available_capacity > 0 and min_age_limit_session == min_age_limit:
                             new_centre['name'] = name
                             new_centre['district_name'] = district_name
                             new_centre['pincode'] = pincode
@@ -64,7 +78,7 @@ def check():
                             new_centre['date'] = date
                             covaxin_centers.append(new_centre)
                             break
-                        elif vaccine == 'COVISHIELD' and available_capacity > 0  and min_age_limit == 18:
+                        elif vaccine == 'COVISHIELD' and available_capacity > 0  and min_age_limit_session == min_age_limit:
                             new_centre['name'] = name
                             new_centre['district_name'] = district_name
                             new_centre['pincode'] = pincode
@@ -73,7 +87,7 @@ def check():
                             new_centre['date'] = date
                             covishield_centers.append(new_centre)
                             break
-                        elif available_capacity > 0 and min_age_limit == 18:
+                        elif available_capacity > 0 and min_age_limit_session == min_age_limit:
                             new_centre['name'] = name
                             new_centre['district_name'] = district_name
                             new_centre['pincode'] = pincode
@@ -86,8 +100,8 @@ def check():
                 print(f'API call failed...')
                 print(f'response status code : {response.status_code}')
                 print(f'response text : {response.text}')
-                time.sleep(15)
-                print(f'Slept for 15 seconds')
+                time.sleep(30)
+                print(f'Slept for 30 seconds')
                 return None, None, None, False
 
     all_centers.extend(covaxin_centers)
@@ -113,13 +127,14 @@ def check():
     return covaxin_df, covishield_df, generic_df, True
 
 
-async def watch(covaxin_df_container, covishield_df_container, generic_df_container, head1, head2, head3, test):
+def write_output(list_of_district_ids, min_age_limit, covaxin_df_container, covishield_df_container, generic_df_container, head1, head2, head3, test):
     while True:
-        covaxin_df, covishield_df, generic_df, is_valid = check()
+        with st.spinner('Loading new data...'):
+            covaxin_df, covishield_df, generic_df, is_valid = check(list_of_district_ids, min_age_limit)
         test.markdown(
             f"""
             <p class="time">
-                Updated at : {str(datetime.now(IST))}
+                Last Updated at : {str(datetime.now(IST).strftime("%d-%m-%Y %H:%M:%S"))} (auto-refreshes)
             </p>
             """, unsafe_allow_html=True)
         if not is_valid:
@@ -130,10 +145,6 @@ async def watch(covaxin_df_container, covishield_df_container, generic_df_contai
                 f"""
                 ## Covaxin Centres
                 """, unsafe_allow_html=True)
-            if covaxin_df.shape[0] > 0:
-                extra.write('# Go go go!! BOOK!!')
-            else:
-                extra.write('')
             covaxin_df_container.dataframe(covaxin_df)
 
             # covishield
@@ -146,17 +157,123 @@ async def watch(covaxin_df_container, covishield_df_container, generic_df_contai
             # covaxin
             head3.markdown(
                 f"""
-                ## Generic Centres
+                ## Generic Centres (Vaccine type not mentioned)
                 """, unsafe_allow_html=True)
             generic_df_container.dataframe(generic_df)
 
-        r = await asyncio.sleep(10)
+        time.sleep(15)
 
+
+###########
+# Layout
+###########
+from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
+from htbuilder.units import percent, px
+from htbuilder.funcs import rgba, rgb
+
+
+def image(src_as_string, **style):
+    return img(src=src_as_string, style=styles(**style))
+
+
+def link(link, text, **style):
+    return a(_href=link, _target="_blank", style=styles(**style))(text)
+
+
+def layout(*args):
+
+    style = """
+    <style>
+      # MainMenu {visibility: hidden;}
+      footer {visibility: hidden;}
+     .stApp { bottom: 70px; }
+    </style>
+    """
+
+    style_div = styles(
+        position="fixed",
+        left=0,
+        bottom=0,
+        margin=px(0, 0, 0, 0),
+        width=percent(100),
+        color="black",
+        text_align="center",
+        height="auto",
+        opacity=1
+    )
+
+    style_hr = styles(
+        display="none"
+    )
+
+    style_p = styles(
+        font_size="12px"
+    )
+
+    body = p(
+        style=style_p
+    )
+    foot = div(
+        style=style_div
+    )(
+        hr(
+            style=style_hr
+        ),
+        body
+    )
+
+    st.markdown(style, unsafe_allow_html=True)
+
+    for arg in args:
+        if isinstance(arg, str):
+            body(arg)
+
+        elif isinstance(arg, HtmlElement):
+            body(arg)
+
+    st.markdown(str(foot), unsafe_allow_html=True)
+
+
+def footer():
+    myargs = [
+        "Made with ❤️ by ",
+        image("https://mk0hootsuiteblof6bud.kinstacdn.com/wp-content/uploads/2018/09/Twitter_Logo_Blue-310x310.png", width=px(25), height=px(25)),
+        link("https://twitter.com/BhavulGauri", "@BhavulGauri"),
+        " (worth a follow?)",
+        br(),
+        "Get yourself and family vaccinated soon. If this tool helps you, kindly donate whatever you can afford to ",
+        link("https://donate.indiacovidresources.in/", "one of the NGOs for covid relief here."),
+    ]
+    layout(*myargs)
+
+
+############
+## MAIN
+############
+
+footer()
+
+district_id_map = get_district_id_map()
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 st.markdown(f"""
-    # Covid Vaccine Slots Availability Checker (18+)
+    # CoWin Vaccine Slots Availability Checker
 
-    Written with love by Bhavul.
+    Designed to help see (almost) realtime updating information about slots that are available.  
+
+    - You can also select multiple districts and see for all of them at once.
+    - The information updates every 30 seconds or so on its own, don't keep refreshing the page.
+    - Results are shown categorised by vaccine types.
+    - Slots for upto 9 days in future are only shown.
+
+    But first, enter your choices in the sidebar on the left, and click "Submit".
     """)
 test = st.empty()
 error = st.empty()
@@ -166,7 +283,6 @@ container2 = st.beta_container()
 container3 = st.beta_container()
 
 head1 = container1.empty()
-extra = container1.empty()
 covaxin_df_container = container1.empty()
 
 head2 = container2.empty()
@@ -175,5 +291,14 @@ covishield_df_container = container2.empty()
 head3 = container3.empty()
 generic_df_container = container3.empty()
 
+st.sidebar.markdown(f"""
+    # Select your choices
+    """)
+radio_age = st.sidebar.radio('Select age limit', ['18+', '45+'])
+options = st.sidebar.multiselect('Select Districts', list(district_id_map.keys()))
 
-asyncio.run(watch(covaxin_df_container, covishield_df_container, generic_df_container, head1, head2, head3, test))
+if st.sidebar.button('Submit'):
+    # actual call which does the job!
+    list_of_district_ids = [district_id_map[district_name] for district_name in options]
+    min_age_limit = 18 if radio_age == '18+' else 45    
+    write_output(list_of_district_ids, min_age_limit, covaxin_df_container, covishield_df_container, generic_df_container, head1, head2, head3, test)
